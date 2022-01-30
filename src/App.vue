bc2a74812294a5c0ab5190e2ab8f6bd881423eda
Rajesh-dot <57223067+Rajesh-dot@users.noreply.github.com>
2022-01-30 12:14:53 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,108 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header @toggle-add-task="toggleTask" title="Problem Tracker" :showAddTask="showAddTask" />
+    <div v-if="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
+import AddTask from './components/AddTask.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Header,
+    Tasks,
+    AddTask,
+  },
+  data() {
+    return {
+      tasks: [],
+      showAddTask: false,
+    }
+  },
+  methods: {
+    addTask(task) {
+      this.tasks = [...this.tasks, task]
+    },
+    deleteTask(id){
+      if (confirm('Are you sure?')){
+        this.tasks = this.tasks.filter((task) => task.id !== id)
+      }
+    },
+    toggleReminder(id){
+      this.tasks = this.tasks.map((task) => task.id===id ? { ...task, reminder: !task.reminder } : task)
+    },
+    toggleTask(){
+      console.log(this.showAddTask)
+      this.showAddTask = !this.showAddTask
+      console.log(this.showAddTask)
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Hello",
+        day: "March 1st at 2:00pm",
+        reminder: false
+      },
+      {
+        id: 2,
+        text: "This should work",
+        day: "March 1st",
+        reminder: true,
+      }
+    ]
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  padding: 30px;
+  border-radius: 5px;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2)
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <div v-if="showAddTask">
+            <Button @btn-click="$emit('toggle-add-task')" text="Close" color="red" />
+        </div>
+        <div v-else>
+            <Button @btn-click="$emit('toggle-add-task')" text="Add Task" color="green" />
+        </div>
+    </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+    export default {
+        name: 'Header',
+        props: {
+            title: String,
+            showAddTask: Boolean,
+        },
+        components: {
+            Button,
+        },
+    }
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
